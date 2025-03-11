17d8563fa4589a8c365b6fddb89fa558d2132ecb
LuckyCodeDog <118164873+LuckyCodeDog@users.noreply.github.com>
2025-03-11 17:02:30 +1300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,10 @@ def create_app():
     db.init_app(app)
 
     from app.dashboard.dashboard import dashboard
+    from app.home.home import home
+    
     app.register_blueprint(dashboard, url_prefix='/dashboard')
+    app.register_blueprint(home, url_prefix='/')
     from app.models.order import Order
     from app.models.user import User
     from app.models.order_item import OrderItem
---
+++ app/home/home.py
@@ -0,0 +1,21 @@
+from flask import Blueprint
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import flash
+from app import db
+from app.models.user import User
+from app.models.order import Order
+from app.common.login_required import login_required
+from app.common.user import current_user  
+
+
+
+home = Blueprint("home", __name__, template_folder="templates")
+
+
+@home.route("/")
+def index():
+    user = current_user()
+    return render_template("home_base.html", user=user)
